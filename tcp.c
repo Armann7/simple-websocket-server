a588c9d4049c6ad429baedcd51c9369bfcbd464f
Slava <vyacheslav.v.kuzmin@gmail.com>
2025-11-22 10:50:35 +0100

    Replace hieroglyph strings by English

--- tcp.c
+++ tcp.c
@@ -6,28 +6,24 @@
 
 int passive_server(int port,int queue)
 {
-    ///定义sockfd
     int server_sockfd = socket(AF_INET,SOCK_STREAM, 0);
 
-    ///定义sockaddr_in
     struct sockaddr_in server_sockaddr;
     server_sockaddr.sin_family = AF_INET;
     server_sockaddr.sin_port = htons(port);
     server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    ///bind，成功返回0，出错返回-1
     if(bind(server_sockfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr))==-1)
     {
         perror("bind");
         exit(1);
     }
-    ///listen，成功返回0，出错返回-1
     if(listen(server_sockfd,queue) == -1)
     {
         perror("listen");
         exit(1);
     }
-    printf("监听%d端口\n",port);
+    printf("Listen to %d\n",port);
     return server_sockfd;
 }
 
